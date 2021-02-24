41a4be9d4d7a5f6b4b21be828aac88a9d1b77b82
MaximSelivanov <max_sell@mail.ru>
2021-02-24 17:39:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    var digit, result = 0
+
+    while( n ){
+        if (n < 0) {n = n*(-1)}
+        digit = n % 10                  
+        result = (result * 10) + digit  
+        n = n/10|0                      
+    }  
+  
+    return result
   
 }
+
+
+
